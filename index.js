02bc5ed5b88b8a70d87dcc41ff71b799024a4ddf
tudorpintea <tudorpintea98@gmail.com>
2020-03-07 13:33:45 +0200

    full-version

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @format
+ */
+// import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//create a component
+const App = () => 
+    (
+       <View style={{ flex: 1 }}> 
+        <Header headerText={'Albums'} />
+        <AlbumList />
+       </View>
+    );
+
+//render it to the device
+AppRegistry.registerComponent('albums', () => App);
